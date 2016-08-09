ad57483f035b78f319f2b9b85c1de1ff54b2a406
Kamoomoo Lucas <KJ@Kamaus-MacBook-Air.local>
2016-08-09 14:53:26 -0500

    updates

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cyanwolf@localhost:5433/flask_movie'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
     return "Hello Flask"
 
-if (__name__) == "__main__":
+if __name__ == "__main__":
     app.run()
